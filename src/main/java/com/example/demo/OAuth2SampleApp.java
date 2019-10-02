0caee23633a53456fbb81746d2803760e016fa31
Osmand Das <osmandpdas@gmail.com>
2019-10-01 22:57:41 -0400

    updated

--- src/main/java/com/example/demo/OAuth2SampleApp.java
+++ src/main/java/com/example/demo/OAuth2SampleApp.java
@@ -1,42 +1,51 @@
 package com.example.demo;
 
+import java.security.Principal;
+
 import javax.annotation.PostConstruct;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableOAuth2Sso
-public class OAuth2SampleApp extends SpringBootServletInitializer {
+@RestController
+public class OAuth2SampleApp extends WebSecurityConfigurerAdapter {
 
-	
+	@RequestMapping("/user")
+	public Principal user(Principal principal) {
+		return principal;
+	}
+
 	/**
-	 * when I am running as a jar this method will be used
+	 * when running as a jar this method will be used
+	 * 
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		//starts here
+		// starts here
 		SpringApplication.run(OAuth2SampleApp.class, args);
 	}
-	
-	
-	/**
-	 * when I am running as a war this method will be used
-	 */
+
 	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-			
-		return builder.sources(OAuth2SampleApp.class);
-	}
-	
+	  protected void configure(HttpSecurity http) throws Exception {
+	    http
+	      .antMatcher("/**")
+	      .authorizeRequests()
+	        .antMatchers("/", "/login**", "/webjars/**", "/error**")
+	        .permitAll()
+	      .anyRequest()
+	        .authenticated();
+	  }
+
 	@PostConstruct
 	public void init() {
-		
-		
+
 	}
-	
-	
+
 }
